12be9c50212859a2f60494e6fd4820ecaa4a173f
JASMIN <JASMIN@DESKTOP-08ECTQN>
2019-04-27 14:42:44 +0530

    Enjoy

---
+++ Quiz-o-mania/src/main/java/com/example/demo/QuizOManiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizOManiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizOManiaApplication.class, args);
+	}
+
+}
